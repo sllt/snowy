a3b74192cb69ed303bbe5892ab8261ba412a8914
Malong <shenlongml@gmail.com>
2013-07-05 08:54:19 +0800

    v0.1

---
+++ lib/utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+
+import re
+
+def find_mentions(content):
+    regex = re.compile(r"@(?P<username>\w+)(\s|$)", re.I)
+    return [m.group("username") for m in regex.finditer(content)]
+
